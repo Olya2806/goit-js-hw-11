122cda850b7125cbc989e853e5e0b551d04d1120
Olya2806 <115026086+Olya2806@users.noreply.github.com>
2023-04-02 22:49:10 +0300

    jlkjhg

--- src/fetchPhoto.js
+++ src/fetchPhoto.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export async function fetchPhoto({searchQuery, page=1}) {
+export async function fetchPhoto(searchQuery, page=1) {
     const IMG_URL = 'https://pixabay.com/api/?key=34995018-f3e2a6c650b6c06aad8f250eb&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}'
 
     try {
