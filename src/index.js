8705fd3625752ae862376ff466f811e8a97e491a
Olya2806 <115026086+Olya2806@users.noreply.github.com>
2023-04-02 22:51:36 +0300

    ;lkjhg

--- src/index.js
+++ src/index.js
@@ -55,6 +55,6 @@ function onCreateGalleryList(image) {
         likes,
         views,
         comments,
-        downloads }) => '<a href="${image.largeImageURL}" class="photo-card gallery__image" data-aos="fade-up"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" clas="galery-image gallery__image" width="280"/><div class="info"><p class="info-item"><b>Likes</b>${image.likes}</p><p class="info-item"><b>Views</b>${image.views}</p><p class="info-item"><b>Comments</b>${image.comments}</p><p class="info-item"><b>Downloads</b>${image.downloads}</p></div></a>')
+        downloads }) => `<a href="${image.largeImageURL}" class="photo-card gallery__image" data-aos="fade-up"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" clas="galery-image gallery__image" width="280"/><div class="info"><p class="info-item"><b>Likes</b>${image.likes}</p><p class="info-item"><b>Views</b>${image.views}</p><p class="info-item"><b>Comments</b>${image.comments}</p><p class="info-item"><b>Downloads</b>${image.downloads}</p></div></a>`)
 }
     
