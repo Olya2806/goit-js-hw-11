e026381dad2ca13e05dd45ebad60c96fe5a97b6a
Olya2806 <115026086+Olya2806@users.noreply.github.com>
2023-04-09 02:41:34 +0300

    add changes

--- src/index.js
+++ src/index.js
@@ -3,28 +3,22 @@ import axios from "axios";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import './css/styles.css';
-// import PixabayAPI from './pixabay-servise';
-import { fetchPhoto } from './fetchPhoto';
 
 const searchingFormEl = document.querySelector('.search-form');
 const galleryListEl = document.querySelector('.js-gallery');
 const loadMoreBtnEl = document.querySelector('.js-load-more');
+const endListText = document.querySelector('.endlist-text')
 
 searchingFormEl.addEventListener('submit', onSearchFormSumit);
-// galleryListEl.addEventListener('click', onGalleryItemClick)
+loadMoreBtnEl.addEventListener('click', onLoadMoreBtnClick);
 
-// const pixabayAPI = new PixabayAPI();
 
-// const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 200, });
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
 
 let baseParams = {
     page: 1,
     name:''
 }
-
-
-let pagesAmount = 0;
-
 
 async function onSearchFormSumit(e) {
     e.preventDefault();
@@ -36,30 +30,52 @@ async function onSearchFormSumit(e) {
         galleryListEl.innerHTML = "";
         return
     }
-
-    // const { data: { hits, totalHits } } = await fetchPhoto(params);
-    // pagesAmount = totalHits;
-    // Notiflix.Notify.success(`We have found ${pagesAmount} photos for you`);
-
-    // if (hits.length === 0) {
-    //     Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again');
-    // }
-    // else {
+    baseParams.page = 1
+    galleryListEl.innerHTML = ('');
+    endListText.classList.add('is-hidden')
 
 
-        fetchPhoto(baseParams.name)
-            .then(({ data }) => {
-                console.log(data.hits);
-                galleryListEl.innerHTML = onCreateGalleryList(data.hits);
-                // galleryListEl.insertAdjacentHTML("beforeend", onCreateGalleryList(data.hits))
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    
+    fetchPhoto(baseParams.name, baseParams.page)
+}
+
+
+
+async function onLoadMoreBtnClick() {
+    fetchPhoto(baseParams.name, baseParams.page)
+}
+
+async function fetchPhoto(searchQuery, page=1) {
+    const IMG_URL = `https://pixabay.com/api/?key=34995018-f3e2a6c650b6c06aad8f250eb&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+
+    try {
+        const { data } = await axios.get(IMG_URL)
+        if (data.total === 0) {
+            galleryListEl.innerHTML = ''
+            loadMoreBtnEl.classList.add('is-hidden')
+            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again');
+            return
+        } 
+        if (baseParams.page === 1) {
+            Notiflix.Notify.success(`We have found ${data.total} photos for you`);
+        }
+            baseParams.page += 1;
+            console.log(data);
+            const html = onCreateGalleryList(data.hits)
+        galleryListEl.insertAdjacentHTML('beforeend', html)
+        loadMoreBtnEl.classList.remove('is-hidden')
+        lightbox.refresh();
+
+        const allPhotos = document.querySelectorAll('.gallery__image')
+        if (data.totalHits === allPhotos.length || allPhotos.length === 480) {
+            loadMoreBtnEl.classList.add('is-hidden')
+            endListText.classList.remove('is-hidden')
+        }
+
+    } catch (error) {
+    console.log(error);
     }
+}
 
-// }
 function onCreateGalleryList(images) {
     return images.map(({
         webformatURL,
@@ -68,18 +84,5 @@ function onCreateGalleryList(images) {
         likes,
         views,
         comments,
-        downloads }) => `<a href="${largeImageURL}" class="photo-card gallery__image" data-aos="fade-up"><img src="${webformatURL}" alt="${tags}" loading="lazy" clas="galery-image gallery__image" width="280"/><div class="info"><p class="info-item"><b>Likes</b>${likes}</p><p class="info-item"><b>Views</b>${views}</p><p class="info-item"><b>Comments</b>${comments}</p><p class="info-item"><b>Downloads</b>${downloads}</p></div></a>`)
-}
-
-loadMoreBtnEl.addEventListener('click', onLoadMoreBtnClick);
-
-async function onLoadMoreBtnClick() {
-    // pixabayAPI.page = pagesAmount.value
-    // pagesAmount += baseParams.page
-
-    
-    // if (pagesAmount <= 0) {
-    //     loadMoreBtnEl.classList.add('is-hidden')
-    // }
-
+        downloads }) => `<a href="${largeImageURL}" class="photo-card gallery__image" data-aos="fade-up"><img src="${webformatURL}" alt="${tags}" loading="lazy" clas="galery-image gallery__image" width="280"/><div class="info"><p class="info-item"><b>Likes</b>${likes}</p><p class="info-item"><b>Views</b>${views}</p><p class="info-item"><b>Comments</b>${comments}</p><p class="info-item"><b>Downloads</b>${downloads}</p></div></a>`).join("")
 }
