bf40e3810db7e9d9af14f1fb526087b6ae4dda24
Olya2806 <115026086+Olya2806@users.noreply.github.com>
2023-04-09 23:29:38 +0300

    add params to fetch

--- src/fetchPhoto.js
+++ src/fetchPhoto.js
@@ -8,7 +8,7 @@ export class PixabayAPI {
     API_KEY = '34995018-f3e2a6c650b6c06aad8f250eb';
     BASE_URL = 'https://pixabay.com/api/';
 
-    async featchPhoto() {
+    async featchPhoto(searchQuery, page=1) {
         try {
             const IMG_URL = await axios.get(`${this.BASE_URL}?key=${this.API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`)
 
